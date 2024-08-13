d6f758e5080d4ad24fb10c2e7d95a7babfb193ab
threshold <gitgudemailaddress@proton.me>
2024-08-13 04:42:55 -0400

    log change detail by default again

--- config/events.js
+++ config/events.js
@@ -3,7 +3,7 @@ const EventTriggers = {
     //this can be modified to add custom behavior based on the post/file data (eg. sending it to another service to check the contents and delete if desired)
     onChange: (chg, board) => {
         console.log(`Local database updated for /${board}/:`); //todo: configurable colors?
-        console.log(chg);
+        console.log(chg.detail);
     }
 };
 export default EventTriggers;
